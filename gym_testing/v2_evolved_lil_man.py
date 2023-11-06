34b7bbea143b4f64906e6d64840311d345af1f42
AverageNormalSchoolBoy <brendan.dufty@gmail.com>
2023-11-06 09:49:14 +0100

    added a tag agent

--- gym_testing/v2_evolved_lil_man.py
+++ gym_testing/v2_evolved_lil_man.py
@@ -10,6 +10,8 @@ from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.env_checker import check_env
 
 
+# I want to add a tag element - which means I have to get rid of the end option
+
     
 
 
@@ -26,7 +28,7 @@ class GoLeftEnv(gym.Env):
     LEFT = 0
     RIGHT = 1
 
-    def __init__(self, width=10, height=10, render_mode="console"):
+    def __init__(self, width=10, height=10, enemy_width=2, enemy_height=8, render_mode="console"):
         super(GoLeftEnv, self).__init__()
         self.render_mode = render_mode
         self.wall = np.array([[]])
@@ -40,7 +42,7 @@ class GoLeftEnv(gym.Env):
         # Define action and observation space
         # They must be gym.spaces objects
         # Example when using discrete actions, we have two: left and right
-        n_actions = 5
+        n_actions = 4
         self.action_space = spaces.Discrete(n_actions)
         # The observation will be the coordinate of the agent
         # this can be described both by Discrete and Box space
@@ -85,29 +87,33 @@ class GoLeftEnv(gym.Env):
                     
                     self.wall=np.append(self.wall, [i,j])
                     total+=1
-                """ 
+                
                 if i in range(wall_min,wall_max) and ( j == self.width//3 or j== 2*self.width//3):
                    self.wall=np.append(self.wall, [j,i])
                    total+=1 
+                """
         self.wall=self.wall.reshape(total, 2)
         
         self.visited = []
         self.seen=[]
         
         self.TagList =[]
-        
+        self.enemy_height=enemy_height
+        self.enemy_width=enemy_width
+        self.TagList=self.MakeTag(enemy_width,enemy_height,self.TagList)
         
     def _get_obs(self):
-        return  np.array([self.agent_pos[0], self.agent_pos[1], self._target_location[0], self._target_location[1]])
+        return_this = np.array([self.agent_pos[0], self.agent_pos[1]])
+        return_this1=[]
+        for enemy in self.TagList:
+            return_this = np.append(return_this, enemy[0])
+            return_this = np.append(return_this, enemy[1])
+        for i in return_this:
+            return_this1.append(i)
+        return_this=np.array(return_this1)
+        return  return_this
     def _get_info(self):
-        info ={"distance": np.linalg.norm(
-                self.agent_pos - self._target_location, ord=1)}
-        for i in self.wall:
-            _id = tuple(i)
-            info[_id]= np.linalg.norm(
-            self.agent_pos - i, ord=1)
-        info["visited"]=self.visited
-        info["seen"]=self.seen
+        info ={}
         return info
     
     def reset(self, seed=None, options=None):
@@ -118,10 +124,10 @@ class GoLeftEnv(gym.Env):
         super().reset(seed=seed, options=options)
         # Initialize the agent at the right of the grid
         self.agent_pos = np.array([3,3])
-        self._target_location = np.array([self.width//2, self.height//2])
+        
         
         # time reward reset
-        self.reward = 0
+        
         self.time_total = 0
         
 
@@ -132,26 +138,50 @@ class GoLeftEnv(gym.Env):
         
         observation = self._get_obs()
         info = self._get_info()
-        self.TagList=[]
-        self.TagList = self.MakeTag(self.width//2 - 3, self.height//2-3, self.TagList)
+        self.TagList =[]
+        self.TagList=self.MakeTag(self.enemy_width,self.enemy_height,self.TagList)
         
+        observation = self._get_obs()
+        info = self._get_info()
         return observation, info  # empty info dict
 
     def step(self, action):
         self.time_total +=1
         reward=0
-        if action ==4:
-            terminated = True
-            return (
-            self._get_obs(),
-            reward,
-            terminated,
-            False,
-            self._get_info(),
-            )
+        terminated = False
+        
+        #have to have the enemies move prior to action 4 - otherwise it learns it can just take action 4 50 times in a row and not get hit
+        """
+        # move the tag guys (but not too often)
+        if self.time_total %2 ==0:
+            self.TagList=self.MoveTag(self.TagList)
+        """
+        # move the tag guys (but give the agent a head start)
+        if self.time_total >= 2:
+            self.TagList=self.MoveTag(self.TagList)
+        # don't let the enemies touch you
+        
+        for enemy in self.TagList:
+            enemy_pos = np.array([enemy[0], enemy[1]])
+            if np.array_equal(enemy_pos, self.agent_pos):
+                reward -= 1000 # once you die get a reward for how long you lived
+                terminated = True
+                return (
+                self._get_obs(),
+                reward,
+                terminated,
+                False,
+                self._get_info(),
+                )
+            else: #get points for being far away
+                reward += abs(enemy_pos[1]-self.agent_pos[1])
+                reward += abs(enemy_pos[0]-self.agent_pos[0])
+        
+       
         direction = self._action_to_direction[action]  
         truncated = False
         agent_new_pos = self.agent_pos + direction
+        
         
         # want to encourage the agent to move to new tiles over visiting old ones 
         
@@ -160,55 +190,28 @@ class GoLeftEnv(gym.Env):
         #but really don't want the agent running into walls over and over again
             
         if self.visited.count(current_cell) >= 1:
-            reward -=15
+            reward-=0
         else:
             self.visited.append(current_cell)
+            reward+=0
+            
+        
         if (self.wall==agent_new_pos).all(1).any():
          #   reward-=1
             pass
         else:
             self.agent_pos = agent_new_pos
         
-        y=self.agent_pos[1]
-        x=self.agent_pos[0]
-        use_seen=np.array(self.seen)
-        #add in line of sight from original agent
-        for i in range(y):
-                if np.any(np.all([x,y-i] ==self.wall , axis=1)): break
-                elif [x,y-i] in self.seen: pass
-                else: 
-                    self.seen.append([x,y-i])
-                    
-                
-        for i in range(self.height-y):
-            if np.any(np.all([x,y+i] == self.wall, axis=1)): break
-            elif [x,y+i] in self.seen: pass
-            else: self.seen.append([x,y+i])   
-        for i in range(x):
-                if np.any(np.all([x-i,y] == self.wall, axis=1)): break
-                elif [x-i,y] in self.seen: pass
-                else: self.seen.append([x-i,y])
-                
-        for i in range(self.width-x):
-            if np.any(np.all([x+i,y] == self.wall, axis=1)): break
-            elif [x+i,y] in self.seen: pass
-            else: 
-                self.seen.append([x+i,y])
-                reward+=1
         
-        terminated = np.array_equal(self.agent_pos, self._target_location)
-        if terminated == True:
-            reward +=5000
-        if self.time_total >= 300:
-            truncated = True
-        
+        if self.time_total >= 200:
+                truncated = True
+                reward += 1000 #big reward for winning
+            
+                
+                
         observation = self._get_obs()
-        
-
-        # Optionally we can pass additional info, we are not using that for now
         info = self._get_info()
-        # move the tag guys
-        self.TagList=self.MoveTag(self.TagList)
+        reward = float(reward)  
         return (
             observation,
             reward,
@@ -222,31 +225,25 @@ class GoLeftEnv(gym.Env):
         use_visited=np.array(self.visited)
         use_seen = np.array(self.seen)
         use_Tag = []
-        print(self.TagList)
+        
         for enemy in self.TagList:
             use_Tag.append([enemy[0],enemy[1]])
         use_Tag = np.array(use_Tag)
-        print("enemy list ", use_Tag)
+       
         if self.render_mode == "console":
             for y in range(self.height):
                 for x in range(self.width):
                     if np.array_equal([x,y], self.agent_pos):
                         print(Fore.MAGENTA + "X", Style.RESET_ALL, end="")
-                    elif np.array_equal([x,y], self._target_location):
-                        print(Fore.GREEN + "0", Style.RESET_ALL, end="")
                     elif np.any(np.all([x,y] == use_Tag, axis=1)):
                         print(Fore.RED + "2", Style.RESET_ALL, end="")
                     elif np.any(np.all([x,y] == self.wall, axis=1)):
                         if np.any(np.all([x,y] == use_visited, axis=1)):
-                            print(Fore.RED+"1",Style.RESET_ALL, end="")
-                        elif np.any(np.all([x,y] == use_seen, axis=1)):
                             print(Fore.BLUE+"1",Style.RESET_ALL, end="")
                         else:
-                            print("1", end=" ")
+                            print(Fore.CYAN+"1", Style.RESET_ALL, end="")
                     else:
                         if np.any(np.all([x,y] == use_visited, axis=1)):
-                            print(Fore.RED+"0",Style.RESET_ALL, end="")
-                        elif np.any(np.all([x,y] == use_seen, axis=1)):
                             print(Fore.BLUE+"0",Style.RESET_ALL, end="")
                         else:
                             print("0", end=" ")
@@ -258,35 +255,47 @@ class GoLeftEnv(gym.Env):
     def MakeTag(self, x,y, TagList):
         stepnumber =0
         direction = 0
-        TagList.append([x,y,direction,stepnumber])
+        TagList.append([x,y])
         #position 
         return TagList
+    
     def MoveTag(self, TagList):
+    # chase the target
         for enemy in TagList:
-            if enemy[3] >4:
-                enemy[2]+=1
-                enemy[2]=enemy[2]%4
-            if enemy[2]==0:
-                enemy[0]+= 1
-            elif enemy[2]==1:
-                enemy[1]+=1
-            elif enemy[2]==2:
-                enemy[0]-=1
-            else:
-                enemy[1]-=1
-            enemy[3]+=1
-        return TagList
+            enemy_move_list = []
+            enemy_pos=np.array(enemy)
+            if np.array_equal(enemy_pos, self.agent_pos):
+                return TagList
+            
+            if self.agent_pos[0] !=enemy[0]:
+                enemy_move_list.append(np.array([(self.agent_pos[0]-enemy[0])/abs(self.agent_pos[0]-enemy[0]), 0]))
+            if self.agent_pos[1] != enemy[1]:
+                enemy_move_list.append(np.array([0, (self.agent_pos[1]-enemy[1])/abs(self.agent_pos[1]-enemy[1])]))
+            
+            direction = random.choice(enemy_move_list)
+            enemy_new_pos=enemy_pos+direction
+            while (self.wall == enemy_new_pos).all(1).any():
+                direction = random.choice([np.array([1,0]),np.array([-1,0]),np.array([0,1]), np.array([0,-1])])
+                enemy_new_pos=enemy_pos+direction
+            enemy[0]=enemy_new_pos[0]
+            enemy[1]=enemy_new_pos[1]
+            
+            
                 
+           
+           
+        return TagList
+          
                 
             
         
 
-env=GoLeftEnv(width=20, height=20)
+env=GoLeftEnv(width=20, height=20,enemy_width=2, enemy_height=18)
 check_env(env, warn=True)
 obs = env.reset()
 env.render()
-"""
-for step in range(1000):
+
+for step in range(50):
     action = env.action_space.sample()  # agent policy that uses the observation and info
     obs, reward, terminated, truncated, info = env.step(action)
     print(f"Step {step + 1}")
@@ -296,13 +305,14 @@ for step in range(1000):
     if done:
         print("Goal reached!", "reward=", reward)
         break
-"""
+        
    
 
 
 
-vec_env = make_vec_env(GoLeftEnv, n_envs=1, env_kwargs=dict(width=20, height=20))
+vec_env = make_vec_env(GoLeftEnv, n_envs=1, env_kwargs=dict(width=20, height=20, enemy_width=2, enemy_height=18))
 model = A2C("MlpPolicy", vec_env, verbose=1)
+
 obs=vec_env.reset()
 vec_env.render()
 eval_env = vec_env
@@ -310,8 +320,9 @@ eval_callback = EvalCallback(eval_env, best_model_save_path="./logs/",
                              log_path="./logs/", eval_freq=500,
                             deterministic=True, render=False)
 
-model.learn(50000)
+model.learn(500000)
 
+vec_env = make_vec_env(GoLeftEnv, n_envs=1, env_kwargs=dict(width=20, height=20, enemy_width=18, enemy_height=18))
 total_reward=np.array([])
 obs = vec_env.reset()
 n_steps = 500
@@ -326,5 +337,5 @@ for step in range(n_steps):
     if done:
         # Note that the VecEnv resets automatically
         # when a done signal is encountered
-        print("Goal reached!", "reward=", np.sum(total_reward))
+        print("Goal reached!", "total reward=", np.sum(total_reward))
         break
